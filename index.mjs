5e2ac0251bae4daf72054a329eee47c6c4b3dbd9
@colecmc <colecmc@correctlydesigned.net>
2024-03-07 10:50:18 -0600

    refactor: Testing actions process
    Additional testing required.
    
    Issue: actions
    
    ---

--- index.mjs
+++ index.mjs
@@ -3,13 +3,14 @@ import { lib } from './src/js/library/lib.mjs';
 /**
  * Y'all want this party started, right?
  * Y'all want this party started quickly, right?
- * @param {String} util
+ * @param {String} utilName
  * @returns {Object}
  */
-function init(util) {
+function init(utilName) {
+    const util = `${utilName}.util`;
     return lib(util);
 }
 
-init('special.util');
+init('special');
 
 export { init };
